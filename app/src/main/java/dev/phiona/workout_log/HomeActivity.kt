ce2524977969d2abe62160fe8f8f8611884195e9
ephy-phiona <ephyphiona@gmail.com>
2022-06-22 21:54:18 +0300

    changes

--- app/src/main/java/dev/phiona/workout_log/HomeActivity.kt
+++ app/src/main/java/dev/phiona/workout_log/HomeActivity.kt
@@ -4,22 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentContainerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import dev.phiona.workout_log.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
-    lateinit var bnvHome:BottomNavigationView
-    lateinit var fvcHome:FragmentContainerView
+   lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding= ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         castView()
         setupBottom()
     }
     fun castView(){
-        bnvHome=findViewById(R.id.bottom_navigation)
-        fvcHome=findViewById(R.id.fcvHome)
+//        bnvHome=findViewById(R.id.bottom_navigation)
+//        fvcHome=findViewById(R.id.fcvHome)
     }
     fun setupBottom(){
-        bnvHome.setOnItemReselectedListener { item->
+       binding.bottomNavigation.setOnItemReselectedListener { item->
             when(item.itemId){             // Plan
                 R.id.page_1->{
                     supportFragmentManager.beginTransaction().replace(R.id.fcvHome,PlanFragment()).commit()
--- app/src/main/java/dev/phiona/workout_log/LoginActivity.kt
+++ app/src/main/java/dev/phiona/workout_log/LoginActivity.kt
@@ -7,46 +7,37 @@ import android.widget.Button
 import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import dev.phiona.workout_log.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    lateinit var btnLogin: Button
-    lateinit var etEmail: TextInputEditText
-    lateinit var etPassword: TextInputEditText
-    lateinit var tilEmail: TextInputLayout
-    lateinit var tilPassword: TextInputLayout
+    lateinit var binding:ActivityLoginBinding
 
-    lateinit var tvSignup: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent= Intent(this,LoginActivity::class.java)
-        setContentView(R.layout.activity_login)
+        binding=ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        btnLogin=findViewById(R.id.btnLogin)
-        etEmail=findViewById(R.id.etEmail1)
-        etPassword=findViewById(R.id.etPassword)
-        tilEmail=findViewById(R.id.tilEmail1)
-        tilPassword=findViewById(R.id.tilPassword)
-        tvSignup=findViewById(R.id.tvSignup)
 
-        tvSignup.setOnClickListener{
+            binding.tvSignup.setOnClickListener{
             val intent= Intent(this,SignupActivity::class.java)
             startActivity(intent)
         }
-        btnLogin.setOnClickListener {
+        binding.btnLogin.setOnClickListener {
             val  intent= Intent(this,HomeActivity::class.java)
             startActivity(intent)
             validate()
         }
     }
     fun validate(){
-        var Email=etEmail.text.toString()
-        var Password=etPassword.text.toString()
+        var Email=binding.etEmail1.text.toString()
+        var Password=binding.etPassword.text.toString()
         if (Email.isBlank()){
-             tilEmail.error="Email is required?"
+             binding.tilEmail1.error="Email is required?"
         }
         if (Password.isBlank()){
-            tilPassword.error="Password is required?"
+            binding.tilPassword.error="Password is required?"
         }
 
 
